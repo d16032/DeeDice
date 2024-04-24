f11da1f6b0159cede306ebe3d36f47647126a625
d136032 <diegogabisonia@gmail.com>
2024-04-24 19:02:04 +0400

    Update rolling effect. Change button style. Optimize code.

--- DeeDice/Controller/ViewController.swift
+++ DeeDice/Controller/ViewController.swift
@@ -6,53 +6,73 @@
 //
 
 import UIKit
-import AVFoundation
 
 class ViewController: UIViewController {
     
-    var audioPlayer : AVAudioPlayer! // import Audio Player lib
     var diceCombo : String! // define the combo string var
     
     var dicee = Dicee() // init the dice from Dice struct
     
+    var diceImages: [UIImage] = []
+    
     @IBOutlet weak var diceImageVideOne: UIImageView! // IB outlet for the left img of dice
     @IBOutlet weak var diceImageViewTwo: UIImageView! // IB outlet for the right img of dice
+    @IBOutlet weak var playerLabel: UILabel!
+    
+    @IBOutlet weak var rollButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        diceImageVideOne.image = UIImage(named: "dice\(1)") // load the first dice with num 1
-        diceImageViewTwo.image = UIImage(named: "dice\(1)") // load the second dice with num 1
-    
+        
+        //Make button rounded
+        rollButton.layer.cornerRadius = rollButton.frame.height / 2
+        
+        rollButton.layer.borderWidth = 4
+        rollButton.layer.borderColor = CGColor(red: 125, green: 48, blue: 48, alpha: 0.5)
+        
+        // Load the images (for demonstration purposes, use images from the bundle)
+        diceImages = dicee.diceImages
+        
+        // Set the initial image
+        diceImageVideOne.image = diceImages[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceImages[Int.random(in: 0...5)]
     }
-
+    
+    // Method to change the image displayed in the UIImageView
+    @objc func changeImage() {
+        
+        // Set the new image
+        let randOne = Int.random(in: 0...5)
+        let randTwo = Int.random(in: 0...5)
+        diceImageVideOne.image = diceImages[randOne]
+        diceImageViewTwo.image = diceImages[randTwo]
+        dicee.d1 = randOne + 1
+        dicee.d2 = randTwo + 1
+    }
+    
     @IBAction func rollButton(_ sender: UIButton) {
         
         dicee.shakeDice() // plays the sfx of shaking dice
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
-            self.updateDice() // rolls the dice after 1 sec of the previous sfx
-        }
+        dicee.imageChangeTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.changeImage), userInfo: nil, repeats: true)
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
+            self.dicee.updateDice() // rolls the dice after 1 sec of the previous sfx
+            self.dicee.stopImageAnimation()
+            self.dicee.dicePlayerTurn(label: self.playerLabel)
+        }
     }
-
     
     override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
         dicee.shakeDice() // plays the sfx of shaking dice
         
+        dicee.imageChangeTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.changeImage), userInfo: nil, repeats: true)
+        
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
-            self.updateDice() // rolls the dice after 1 sec of the previous sfx
+            self.dicee.updateDice() // rolls the dice after 1 sec of the previous sfx
+            self.dicee.stopImageAnimation()
+            self.dicee.dicePlayerTurn(label: self.playerLabel)
         }
     }
-    
-    func updateDice() {
-        let diceNumberOne = dicee.rollDiceLeft() // gets the number of the left dice
-        let diceNumberTwo = dicee.rollDiceRight() // gets the number of the right dice
-        
-        diceImageVideOne.image = UIImage(named: "dice\(diceNumberOne)") // updates the image of the left dice
-        diceImageViewTwo.image = UIImage(named: "dice\(diceNumberTwo)") // updates the image of the right dice
-        
-        dicee.doPlay(combo: "comb-\(diceNumberOne)x\(diceNumberTwo)") // plays the sound of the dice combination
-    }
-    
 }
 
--- DeeDice/Model/Model.swift
+++ DeeDice/Model/Model.swift
@@ -5,14 +5,35 @@
 //  Created by d1e6032 on 4/10/24.
 //
 
-import Foundation
+import UIKit
 import AVFoundation
 
 var audioPlayer : AVAudioPlayer!
 
 struct Dicee {
-
+    
     var diceCombo: String!
+    var diceTrhow = 0
+    var p1History: [String] = []
+    var p2History: [String] = []
+    var d1 = 0
+    var d2 = 0
+    
+    // Timer to control the speed of image changes
+    var imageChangeTimer: Timer?
+    
+    // Create an instance of UIImageView to display the images
+    var imageView: UIImageView!
+    
+    // ARRAY OF DICE IMAGES
+    var diceImages = [
+        UIImage(named: "dice1")!,
+        UIImage(named: "dice2")!,
+        UIImage(named: "dice3")!,
+        UIImage(named: "dice4")!,
+        UIImage(named: "dice5")!,
+        UIImage(named: "dice6")!
+    ]
     
     func rollDiceLeft() -> Int {                    // gets and returns the random num for the left dice
         let leftDice = Int.random(in: 1...6)
@@ -24,6 +45,7 @@ struct Dicee {
         return rightDice
     }
     
+    // AUDIO FOR SHAKING DICE
     func shakeDice() {                              // sft for shaking dice
         let rollDice = Bundle.main.url(forResource: "diceRoll", withExtension: "mp3")
         do {
@@ -36,7 +58,8 @@ struct Dicee {
         }
         audioPlayer.play()
     }
-
+    
+    // AUDIO FOR COMBOS
     mutating func doPlay(combo:String) {
         let combinations = Bundle.main.url(forResource: combo, withExtension: "mp3")
         do {
@@ -48,5 +71,39 @@ struct Dicee {
             print(error)
         }
         audioPlayer.play()
+    }
+    
+    mutating func dicePlayerTurn(label: UILabel) {
+        if diceTrhow == 0 {
+            self.diceTrhow += 1
+            label.text = "Player 2 turn"
+        } else {
+            self.diceTrhow -= 1
+            label.text = "Player 1 turn"
+        }
+    }
+    
+    // UPDADE DICE IMAGES FROM RANDOM NUMBERS
+    mutating func updateDice() {
+        let diceNumberOne = d1 // gets the number of the left dice
+        let diceNumberTwo = d2 // gets the number of the right dice
+        //dice1.image = UIImage(named: "dice\(diceNumberOne)") // updates the image of the left dice
+        //dice2.image = UIImage(named: "dice\(diceNumberTwo)") // updates the image of the right dice
+        if diceTrhow == 0 {
+            p1History.append("\(diceNumberOne)x\(diceNumberTwo)")
+        } else {
+            p2History.append("\(diceNumberOne)x\(diceNumberTwo)")
+        }
+        print("Player 1: \(p1History)")
+        print("Player 2: \(p2History)")
+        
+        doPlay(combo: "comb-\(diceNumberOne)x\(diceNumberTwo)") // plays the sound of the dice combination
+        
+    }
+    
+    mutating func stopImageAnimation() {
+        // Invalidate the timer to stop the image animation
+        imageChangeTimer?.invalidate()
+        self.imageChangeTimer = nil // Clear the reference to the timer
     }
 }
